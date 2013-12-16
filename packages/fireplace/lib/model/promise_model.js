4051f58eec67db45f76a5b6d1fac3c9b747c1b47
Richard Livsey <richard@livsey.org>
2013-12-16 16:23:12 +0000

    PromiseModel forwards functions to its content

--- packages/fireplace/lib/model/promise_model.js
+++ packages/fireplace/lib/model/promise_model.js
@@ -1,3 +1,24 @@
+var get = Ember.get;
+
 FP.PromiseModel = Ember.ObjectProxy.extend(Ember.PromiseProxyMixin, {
+
+  // forward on all content's functions where it makes sense to do so
+  _setupContentForwarding: function() {
+    var obj = get(this, "content");
+    if (!obj) { return; }
+
+    for (var prop in obj) {
+      if (!this[prop] && typeof obj[prop] === "function") {
+        this._forwardToContent(prop);
+      }
+    }
+  }.observes("content").on("init"),
+
+  _forwardToContent: function(prop) {
+    this[prop] = function() {
+      var content = this.get("content");
+      return content[prop].apply(content, arguments);
+    };
+  }
 
 });
