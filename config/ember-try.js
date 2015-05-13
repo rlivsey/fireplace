89776a648dded1b47983c1e9a302034078aa3671
Richard Livsey <richard@livsey.org>
2015-05-14 00:49:40 +0100

    pin ember-try to canary for now

---
+++ config/ember-try.js
@@ -0,0 +1,28 @@
+module.exports = {
+  scenarios: [
+    {
+      name: "default",
+      dependencies: { } // no dependencies needed as the
+                        // default is already specified in
+                        // the consuming app's bower.json
+    },
+    // {
+    //   name: "ember-release",
+    //   dependencies: {
+    //     "ember": "release"
+    //   }
+    // },
+    // {
+    //   name: "ember-beta",
+    //   dependencies: {
+    //     "ember": "beta"
+    //   }
+    // },
+    // {
+    //   name: "ember-canary",
+    //   dependencies: {
+    //     "ember": "canary"
+    //   }
+    // }
+  ]
+};
